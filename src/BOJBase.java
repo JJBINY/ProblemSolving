af6c0cd05c2ce27ad4b3e7541f154b0ec4e70acf
JJBINY <sisoya0424@gmail.com>
2024-03-22 18:28:04 +0900

    BOJ move package

---
+++ src/BOJBase.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+import static java.lang.Integer.parseInt;
+
+
+/**
+ *
+ */
+public class BOJBase {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            solve(br);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void solve(BufferedReader br) throws IOException {
+
+
+    }
+}
