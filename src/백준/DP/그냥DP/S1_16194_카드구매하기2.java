3dcfddacc6e2e7603848587e12edf05eb83d96d7
JJBINY <sisoya0424@gmail.com>
2024-05-29 16:00:43 +0900

    BOJ 16194

---
+++ src/백준/DP/그냥DP/S1_16194_카드구매하기2.java
@@ -0,0 +1,48 @@
+package 백준.DP.그냥DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+/**
+ * S1_16194_카드구매하기2
+ * DP
+ */
+public class S1_16194_카드구매하기2 {
+    static StringBuilder ans = new StringBuilder();
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int T = 1;
+//            int T = parseInt(br.readLine());
+            for (int i = 1; i <= T; i++) {
+                ans.append(solve(br));
+                ans.append("\n");
+            }
+            System.out.print(ans);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static Object solve(BufferedReader br) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] p = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = i; j <= N; j++) {
+                dp[j] = Math.min(dp[j], dp[j - i] + p[i]);
+            }
+        }
+        return dp[N];
+    }
+}
