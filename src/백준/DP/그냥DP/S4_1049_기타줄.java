2f514bc9c4cadc20b2c9540ba2d519166ef3b6ac
JJBINY <sisoya0424@gmail.com>
2024-07-09 21:33:25 +0900

    BOJ 1049

---
+++ src/백준/DP/그냥DP/S4_1049_기타줄.java
@@ -0,0 +1,52 @@
+package 백준.DP.그냥DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+
+
+/**
+ * S4_1049_기타줄
+ * DP
+ */
+public class S4_1049_기타줄 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder ans = new StringBuilder();
+            int T = 1;
+//            int T = parseInt(br.readLine());
+            for (int i = 1; i <= T; i++) {
+                ans.append(solve(br));
+                ans.append("\n");
+            }
+            System.out.print(ans);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static Object solve(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = parseInt(st.nextToken());
+        int M = parseInt(st.nextToken());
+        int[] arr = new int[M];
+        int ea = Integer.MAX_VALUE;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = parseInt(st.nextToken());
+            ea = Math.min(ea, parseInt(st.nextToken()));
+        }
+
+        int[] dp = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            dp[i] = dp[i - 1] + ea;
+            for (int j = 0; j < M; j++) {
+                dp[i] = Math.min(dp[i], dp[Math.max(0,i - 6)] + arr[j]);
+            }
+        }
+        return dp[N];
+    }
+}
