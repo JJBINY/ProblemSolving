94d2c5542cb421df74ff45a7fc8de926bc2d3ba5
JJBINY <sisoya0424@gmail.com>
2025-02-10 21:06:41 +0900

    BOJ: 14430

---
+++ src/백준/DP/그냥DP/tabulation/S2_14430_자원_캐기.java
@@ -0,0 +1,44 @@
+package 백준.DP.그냥DP.tabulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+/**
+ * S2_14430_자원_캐기
+ * DP
+ */
+public class S2_14430_자원_캐기 {
+
+    static Object solve(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[N+1][M+1];
+        for (int i = 1; i < N+1; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < M+1; j++) {
+                dp[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] += Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[N][M];
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder ans = new StringBuilder();
+            int T = 1;
+//            int T = Integer.parseInt(br.readLine());
+            for (int i = 1; i <= T; i++) {
+                ans.append(solve(br));
+                ans.append("\n");
+            }
+            System.out.print(ans);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
